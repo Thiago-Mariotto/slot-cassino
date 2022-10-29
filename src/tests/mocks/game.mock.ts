fd4e7a2a2273559aef9a3800f6b7a4876615d2cd
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-10-29 09:57:46 -0300

    test: add game mock to test

---
+++ src/tests/mocks/game.mock.ts
@@ -0,0 +1,9 @@
+import GameType from '../../types/GameTypes';
+import { figures } from './figures.mock';
+
+export const gameMock: GameType = {
+  name: 'Game of Food',
+  figures: figures,
+  xSize: 6,
+  ySize: 5
+}
